5f8873568d80e1bb4fc1e03a92c8dad821698d9f
bossSteve03 <stotev0@gmail.com>
2023-05-03 08:54:03 +0100

    sidebar fixed as there was issues, needs content to fill in and i want to make it collapsable NO bootstrap for sidebar

--- client/src/layouts/SideNav/index.jsx
+++ client/src/layouts/SideNav/index.jsx
@@ -1,53 +1,30 @@
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import './index.modules.css'
+  
 
-export default function SideNav() {
+export default function Sidebar() {
 
+  const [open, setOpen] = useState(false);
+  
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
   return (
     <>
-    <div className="d-flex flex-column flex-shrink-0 bg-light" style={{ width: '4.5rem' }}>
-      <Navbar expand="lg" className="flex-column">
-        <Navbar.Brand href="/" className="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-          <svg className="bi" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
-          <span className="visually-hidden">Icon-only</span>
-        </Navbar.Brand>
-        <Nav className="nav-pills nav-flush flex-column mb-auto text-center">
-          <Nav.Link href="#" className="nav-link py-3 border-bottom" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-            <svg className="bi" width="24" height="24" role="img" aria-label="Home"><use xlinkHref="#home"></use></svg>
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-            <svg className="bi" width="24" height="24" role="img" aria-label="Dashboard"><use xlinkHref="#speedometer2"></use></svg>
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
-            <svg className="bi" width="24" height="24" role="img" aria-label="Orders"><use xlinkHref="#table"></use></svg>
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
-            <svg className="bi" width="24" height="24" role="img" aria-label="Products"><use xlinkHref="#grid"></use></svg>
-          </Nav.Link>
-          <Nav.Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
-            <svg className="bi" width="24" height="24" role="img" aria-label="Customers"><use xlinkHref="#people-circle"></use></svg>
-          </Nav.Link>
-        </Nav>
-        <NavDropdown title={<img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" className="rounded-circle" />} className="dropdown border-top" alignRight>
-          <NavDropdown.Item href="#">New project...</NavDropdown.Item>
-          <NavDropdown.Item href="#">Settings</NavDropdown.Item>
-          <NavDropdown.Item href="#">Profile</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#">Sign out</NavDropdown.Item>
-        </NavDropdown>
-      </Navbar>
+    <div id="sidebar">
+      <h3>Sidebar</h3>
+      <ul>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+      </ul>
     </div>
-    <Outlet />
+    <div id="content">
+      <Outlet />
+    </div>
     </>
   );
-};
+}
