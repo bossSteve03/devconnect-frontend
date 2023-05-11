4168970b3752cd79b0a0073d5d7f66510edfc9a6
bossSteve03 <stotev0@gmail.com>
2023-05-11 16:22:17 +0100

    styling to dashboard and added scroll to overflow in sidebar content container

--- client/src/components/DashboardComponents/HN/index.jsx
+++ client/src/components/DashboardComponents/HN/index.jsx
@@ -1,18 +1,18 @@
-import '../dashboardTasks.css';
+import styles from '../index.module.css';
 
 export default function HasName() {
 
   return (
-    <div className="containerBorderH">
-      <div className="messageContainer">
-        <h1 className="messageTitleN">
+    <div className={styles["containerBorderH"]}>
+      <div className={styles["messageContainer"]}>
+        <h1 className={styles["messageTitleN"]}>
         Task complete.
         </h1>
-        <p className="messageContentN">
-          <strong className="messageContentN">
+        <p className={styles["messageContentN"]}>
+          <strong className={styles["messageContentN"]}>
           Name
           </strong>
-        successfully added to User Profile.
+         successfully added to User Profile.
         </p>
       </div>
     </div>
